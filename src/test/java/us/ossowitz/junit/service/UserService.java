4b8687b5955c78acf58bfef379343ce1dbf19bdb
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-06-16 22:27:40 +0300

    FirstSimpleJUnitTest

---
+++ src/test/java/us/ossowitz/junit/service/UserService.java
@@ -0,0 +1,13 @@
+package us.ossowitz.junit.service;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class UserService {
+
+    @Test
+    void test() {
+        fail();
+    }
+}
