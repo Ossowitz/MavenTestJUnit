a96d66e2c7d6223eff1a7ed901a9e610c374873a
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-06-18 21:32:17 +0300

    @BeforeEach and @BeforeAll

--- src/main/java/us/ossowitz/junit/service/UserService.java
+++ src/main/java/us/ossowitz/junit/service/UserService.java
@@ -2,11 +2,17 @@ package us.ossowitz.junit.service;
 
 import us.ossowitz.junit.dto.User;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
+    private final List<User> users = new ArrayList<>();
+
     public List<User> getAll() {
-        return Collections.emptyList();
+        return users;
+    }
+
+    public boolean add(User user) {
+        return users.add(user);
     }
 }
--- src/test/java/us/ossowitz/junit/service/UserServiceTest.java
+++ src/test/java/us/ossowitz/junit/service/UserServiceTest.java
@@ -1,16 +1,41 @@
 package us.ossowitz.junit.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import us.ossowitz.junit.dto.User;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTest {
+    private UserService userService;
+
+    @BeforeEach
+    public void prepare() {
+        System.out.println("Before each: " + this);
+        userService = new UserService();
+    }
 
     @Test
     public void usersEmptyIfNoUserAdded() {
-        var userService = new UserService();
+        System.out.println("Test 1: " + this);
         var users = userService.getAll();
-        assertFalse(users.isEmpty(), () -> "User list should be empty");
+        assertTrue(users.isEmpty(), () -> "User list should be empty");
+    }
+
+    @Test
+    public void usersSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
+
+        userService.add(new User());
+        userService.add(new User());
+
+        var users = userService.getAll();
+        assertEquals(2, users.size());
+    }
+
+    @AfterEach
+    public void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
     }
 }
