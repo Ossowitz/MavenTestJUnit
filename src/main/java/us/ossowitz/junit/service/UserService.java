588f0e07fb3da2d8fa1769a0597c4e49a74c9652
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-06-28 18:38:21 +0300

    Assertions

--- src/main/java/us/ossowitz/junit/service/UserService.java
+++ src/main/java/us/ossowitz/junit/service/UserService.java
@@ -2,17 +2,12 @@ package us.ossowitz.junit.service;
 
 import us.ossowitz.junit.dto.User;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class UserService {
-    private final List<User> users = new ArrayList<>();
 
     public List<User> getAll() {
-        return users;
-    }
-
-    public boolean add(User user) {
-        return users.add(user);
+        return Collections.emptyList();
     }
 }
--- src/test/java/us/ossowitz/junit/service/UserServiceTest.java
+++ src/test/java/us/ossowitz/junit/service/UserServiceTest.java
@@ -1,64 +1,18 @@
 package us.ossowitz.junit.service;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import us.ossowitz.junit.dto.User;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
-    private UserService userService;
-
-    @BeforeAll
-    public void init() {
-        System.out.println("Before all: " + this);
-    }
-
-    @BeforeEach
-    public void prepare() {
-        System.out.println("Before each: " + this);
-        userService = new UserService();
-    }
 
     @Test
-    public void usersEmptyIfNoUserAdded() {
-        System.out.println("Test 1: " + this);
-        var users = userService.getAll();
-        assertTrue(users.isEmpty(), () -> "User list should be empty");
-    }
-
-    @Test
-    public void usersSizeIfUserAdded() {
-        System.out.println("Test 2: " + this);
-
-        userService.add(new User());
-        userService.add(new User());
-
-        var users = userService.getAll();
-        assertEquals(2, users.size());
-    }
-
-    @AfterEach
-    public void deleteDataFromDatabase() {
-        System.out.println("After each: " + this);
-    }
-
-    @AfterAll
-    public void closeConnectionPool() {
-        System.out.println("After all: " + this);
-    }
-
-    @Test
-    public void listConnection() {
-        List<Integer> expected = Arrays.asList(1, 2, 3);
-        List<Integer> actual = Arrays.asList(3, 2, 1);
-
-
+    void usersEmptyIfNoUserAdded() {
+        UserService userService = new UserService();
+        List<User> users = userService.getAll();
+        assertTrue(users.isEmpty());
     }
 }
